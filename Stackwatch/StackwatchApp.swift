1b67ddf905d9221b33fa29e4cffe659af74cea7d
yujingaya <yujingaya@linecorp.com>
2022-11-19 18:04:28 +0900

    Define window style and resize policy

--- Stackwatch/StackwatchApp.swift
+++ Stackwatch/StackwatchApp.swift
@@ -5,6 +5,12 @@ struct StackwatchApp: App {
     var body: some Scene {
         DocumentGroup(newDocument: StackwatchDocument()) { file in
             ContentView(document: file.$document)
+                .frame(minWidth: 300, maxWidth: 600)
         }
+        #if os(macOS)
+        .windowResizability(.contentSize)
+        .windowStyle(.hiddenTitleBar)
+        .windowToolbarStyle(.unifiedCompact)
+        #endif
     }
 }

8a0e3e5aaf51702196ac24fa30a1f95e4d02f6dd
yujingaya <yujingaya@linecorp.com>
2022-11-19 16:39:04 +0900

    Define operations on document

--- Stackwatch/Model/StackwatchDocument.swift
+++ Stackwatch/Model/StackwatchDocument.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct StackwatchDocument: Codable {
     var title: String
-    var dones: [Done]
-    var todos: [Todo]
+    private(set) var dones: [Done]
+    private(set) var todos: [Todo]
 
     init(title: String = "New Stack!") {
         self.title = title
@@ -12,9 +12,32 @@ struct StackwatchDocument: Codable {
     }
 }
 
+extension StackwatchDocument {
+    mutating func appendTodo(_ todo: Todo) {
+        todos.append(todo)
+    }
+
+    mutating func done(with duration: TimeInterval) {
+        assert(!todos.isEmpty)
+
+        let todo = todos.removeFirst()
+        let done = Done(todo, duration: duration)
+        dones.append(done)
+    }
+
+    var totalDuration: TimeInterval {
+        dones.map { $0.duration }.reduce(0, +)
+    }
+}
+
 struct Done: Codable {
     var name: String
     var duration: TimeInterval
+
+    init(_ todo: Todo, duration: TimeInterval) {
+        self.name = todo.name
+        self.duration = duration
+    }
 }
 
 struct Todo: Codable {
